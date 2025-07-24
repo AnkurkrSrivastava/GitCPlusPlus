588abfefdfb2f5a5ea89a3a1c78e1f2d2cec0e18
AnkurkrSrivastava <ankur821305@gmail.com>
2025-07-24 23:58:36 +0530

    Book Allocation problem

---
+++ Problems/roughhh.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    for (int i = 0; i <= 52; i++)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+    int mid = (0+53)/2;
+    cout << mid ;
+    
+    return 0;
+}
