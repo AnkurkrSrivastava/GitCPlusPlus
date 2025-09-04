72a5ed8907d5eaf37dbcdff882486ffefa41893d
AnkurkrSrivastava <ankur821305@gmail.com>
2025-09-04 12:13:12 +0530

    Leet Problem 1910

---
+++ Problems/RemoveAllOccurrences.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+string Remove_Occurrences(string s, string part){
+    while (s.length()>0 && s.find(part) < s.length())
+    {
+        s.erase(s.find(part),part.length());
+    }
+    return s;
+}
+
+int main(){
+    string s = "daabcbaabcbc";
+    string part = "abc";
+    cout << Remove_Occurrences(s,part) << endl;
+    return 0;
+}
