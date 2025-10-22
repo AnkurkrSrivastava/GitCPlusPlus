7d250b6ba8c98539b194a2892467e935f25e3000
AnkurkrSrivastava <ankur821305@gmail.com>
2025-10-22 14:49:27 +0530

    Recursion basics

---
+++ Recursion and BT/NSum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int NSum(int num){
+    if(num == 1){
+        return 1;
+    }
+    return num + NSum(num-1);
+}
+
+int main(){
+    cout << NSum(4) << endl;
+    return 0;
+}
