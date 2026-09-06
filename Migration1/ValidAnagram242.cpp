a8b01019defb8f61ffb8ad5c53a2c0689b8ca75c
AnkurkrSrivastava <ankur821305@gmail.com>
2026-09-07 01:30:14 +0530

    leet problem

---
+++ Migration1/ValidAnagram242.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isAnagram(string &s, string &t){
+    if (s.length() != t.length())
+    {
+        return false;
+    }
+    int freq[26] = {0};
+    for (int i = 0; i < s.length(); i++)
+    {
+        freq[s[i]-'a']++;
+        freq[t[i]-'a']--;
+    }
+    for (int i = 0; i < 26; i++)
+    {
+        if (freq[i] != 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+    string a = "cat";
+    string b = "nagaram";
+    cout << boolalpha << isAnagram(a,b);
+    return 0;
+}
