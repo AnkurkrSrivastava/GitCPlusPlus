ae95468935ef9e61c281d3759129ee317edbef2d
AnkurkrSrivastava <ankur821305@gmail.com>
2026-09-08 23:01:13 +0530

    inserting linked list

---
+++ Linked List/LLinsertion.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+};
+
+void insertbeginning(Node* &head, int value){
+    Node* newNode = new Node;
+    newNode -> data = value;
+    newNode -> next = head;
+    head = newNode;
+}
+
+void transverse(Node* head){
+    Node* temp = head;
+    while (temp != NULL)
+    {
+        cout << temp -> data << " ";
+        temp = temp -> next; 
+    }
+    
+}
+
+int main(){
+    Node* first = new Node;
+    Node* second = new Node;
+    Node* third = new Node;
+    
+    first -> data = 10;
+    first -> next = second;
+    second -> data = 20;
+    second -> next = third;
+    third -> data = 30;
+    third -> next = NULL;
+    insertbeginning(first,15);
+    transverse(first);
+    return 0;
+}
