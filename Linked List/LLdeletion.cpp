8b3bb0d5993193e2e58d3ec07c15bd8d73a04e3a
AnkurkrSrivastava <ankur821305@gmail.com>
2026-09-09 12:13:26 +0530

    deleting linked list

---
+++ Linked List/LLdeletion.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+};
+
+void deletionSt(Node* &head){
+    if (head == NULL)
+    {
+        cout << "Empty List" << endl;
+        return;
+    }
+    Node* temp = head;
+    head = head -> next;
+    delete temp;
+    
+}
+
+void transverse(Node* head){
+    Node* temp = head;
+    while (temp != NULL)
+    {
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    
+}
+
+int main(){
+    Node* first = new Node;
+    Node* second = new Node;
+    Node* third = new Node;
+    first -> data = 10;
+    first -> next = second;
+
+    second -> data = 20;
+    second -> next = third;
+
+    third -> data = 30;
+    third -> next = NULL;
+    
+    deletionSt(first);
+    transverse(first);
+}
