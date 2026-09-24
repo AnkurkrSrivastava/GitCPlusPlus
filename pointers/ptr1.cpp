7e748ab3802082908bef0057760df82bee68ce5d
AnkurkrSrivastava <ankur821305@gmail.com>
2026-09-25 00:37:58 +0530

    practice 3

---
+++ pointers/ptr1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int largest = 0;
+    int smallest = INT16_MAX;
+    int arr[5];
+    cout << "Enter 5 numbers: ";
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+    }
+    int *p = arr;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << *(p+i) << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        largest = max(*(p+i),largest);
+        smallest = min(*(p+i),smallest);
+    }
+    cout << "Largest: " << largest << endl;
+    cout << "Smallest: " << smallest << endl;
+    
+    
+    return 0;
+}
