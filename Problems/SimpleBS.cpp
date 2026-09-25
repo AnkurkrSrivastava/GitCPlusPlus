db5511fa00841c0ca9f87ec0ac6ef12ca0d182ca
AnkurkrSrivastava <ankur821305@gmail.com>
2026-09-25 10:30:32 +0530

    practice 4

--- Problems/SimpleBS.cpp
+++ Problems/SimpleBS.cpp
@@ -2,15 +2,23 @@
 using namespace std;
 
 int binarySearch(int arr[], int n, int key) {
-    for (int i = 0; i < n; i++)
+    int st = 0;
+    int end = n-1;
+    while (st <= end)
     {
-        if (arr[i] == key)
+        int mid = st+(end-st)/2;
+        if (arr[mid] == key)
         {
-            return i;
+            return mid;
         }
-        
+        else if (key < arr[mid])
+        {
+            end = mid - 1;
+        }else{
+            st = mid + 1;
+        }
     }
-    
+    return -1;
 }
 
 int main() {
